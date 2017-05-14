353b21551a8532546670e9abc891b83f247e902b
Ruth Earle <codeeva@moneymaker.home>
2017-05-14 12:47:27 +0100

    initial commit. app wired with webpack. next wire test

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import angular from 'angular';
+
+angular.module('app', [])
+  .controller('ProductListController', ProductListController);
+
+function ProductListController() {
+  this.name = 'Hello';
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import '../node_modules/basscss/css/basscss.min.css'
+
+import './assets/styles/global.css';
+
+import './index.html';
+import './app.js'
+
+
